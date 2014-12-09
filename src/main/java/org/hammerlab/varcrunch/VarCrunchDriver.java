063b8bdea773842db0c3c1fde8bb5bc7037cdd31
arahuja <aahuja11@gmail.com>
2014-12-09 14:35:39 -0500

    Adding single driver class

---
+++ src/main/java/org/hammerlab/varcrunch/VarCrunchDriver.java
@@ -0,0 +1,26 @@
+package org.hammerlab.varcrunch;
+
+import org.apache.hadoop.util.ProgramDriver;
+import org.hammerlab.varcrunch.pipelines.ComputeReadDepthInInterval;
+import org.hammerlab.varcrunch.pipelines.GermlineVarCrunch;
+import org.hammerlab.varcrunch.pipelines.SomaticVarCrunch;
+
+public class VarCrunchDriver {
+
+    public static void main(String[] args) throws Exception {
+
+        ProgramDriver programDriver = new ProgramDriver();
+        int exitCode = -1;
+        try {
+            programDriver.addClass("readdepth", ComputeReadDepthInInterval.class, "Computes read depth over a given size interval");
+            programDriver.addClass("germline", GermlineVarCrunch.class, "Standard germline variant caller");
+            programDriver.addClass("somatic", SomaticVarCrunch.class, "Standard somatic variant caller, takes tumor/normal input");
+
+            exitCode = 0;
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
+        System.exit(exitCode);
+    }
+}
