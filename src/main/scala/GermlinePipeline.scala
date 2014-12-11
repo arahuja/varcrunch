3fdbf78ba972445286828d4db4da8b5a15f368e8
arahuja <aahuja11@gmail.com>
2014-12-11 10:40:17 -0500

    Add guacamole and adam dependencies; utilize variant calling from guacamole in scrunch caller;

--- src/main/scala/GermlinePipeline.scala
+++ src/main/scala/GermlinePipeline.scala
@@ -1,20 +1,32 @@
 package org.hammerlab.pipeline
 
-import fi.tkk.ics.hadoop.bam.{AnySAMInputFormat, SAMRecordWritable}
 import htsjdk.samtools.SAMRecord
-import org.apache.crunch.scrunch.{PCollection, Writables, PipelineApp}
+import org.apache.crunch.scrunch.{PCollection, PipelineApp, Writables}
 import org.apache.hadoop.io.LongWritable
+import org.bdgenomics.formats.avro.Genotype
+import org.hammerlab.guacamole.commands.GermlineStandard
+import org.hammerlab.guacamole.pileup.Pileup
+import org.hammerlab.guacamole.reads.{MappedRead, Read}
+import org.hammerlab.guacamole.variants.AlleleConversions
+import org.hammerlab.guacamole.windowing.SlidingWindow
+import org.seqdoop.hadoop_bam.{AnySAMInputFormat, SAMRecordWritable}
+
+import scala.collection.mutable.ArrayBuffer
 
 object GermlinePipeline extends PipelineApp {
+
+  type GenomicPosition = (String, Int)
+
   override def run(args: Array[String]): Unit = {
 
     val inputPath = args(0)
+    val outputPath = args(1)
 
     val reads: PCollection[SAMRecordWritable] = read(from.formattedFile(
       inputPath,
       classOf[AnySAMInputFormat],
-      Writables.writables[LongWritable],
-      Writables.writables[SAMRecordWritable]
+      classOf[LongWritable],
+      classOf[SAMRecordWritable]
     )).values()
 
     // Filter to mapped reads
@@ -24,15 +36,48 @@ object GermlinePipeline extends PipelineApp {
     })
 
     // Compute depth in interval
-    val readsInIntervals: PCollection[(String, Int)] = mappedReads.flatMap( r => {
+    val readsInIntervals: PCollection[(String, Int)] = mappedReads.flatMap(r => {
       val intervalSize = 100000
       val read: SAMRecord = r.get()
-      var lastInterval = -1;
       val overlappingIntervals = (read.getAlignmentStart until read.getAlignmentEnd).map( _ / intervalSize).toSet
-      overlappingIntervals.map((read.getReferenceName, _))
-    })
+
+      overlappingIntervals.map(pos => (read.getReferenceName, pos))
+    }, Writables.tuple2(Writables.strings, Writables.ints))
 
     val intervalCounts = readsInIntervals.count()
 
+    // Partition reads to overlapping intervals, duplicate reads if they overlap many intervals
+    val readsPartitioned = mappedReads.flatMap(r => {
+      val intervalSize = 100000
+      val read: SAMRecord = r.get()
+      val overlappingIntervals = (read.getAlignmentStart until read.getAlignmentEnd).map( _ / intervalSize).toSet
+      overlappingIntervals.map( (_, (read.getAlignmentStart, r)) )
+    })
+
+    val genotypes = readsPartitioned.secondarySortAndFlatMap(callVariants)
+
+    write(genotypes, to.avroFile(outputPath))
+
+  }
+
+  def callVariants(task: Int, readsAndPositions: Iterable[(Int, SAMRecordWritable)]): Seq[Genotype] = {
+
+    val readsAndPositionsIterator = readsAndPositions.iterator
+    val reads: Iterator[MappedRead] = readsAndPositionsIterator.flatMap(kv => Read.fromSAMRecordOpt(kv._2.get(), 0).flatMap(_.getMappedReadOpt))
+    val window = SlidingWindow(0L, reads)
+    var nextLocus = window.nextLocusWithRegions()
+
+    val genotypes = new ArrayBuffer[Genotype]()
+
+    while (nextLocus.isDefined) {
+      window.setCurrentLocus(nextLocus.get)
+      val pileup = Pileup(window.currentRegions(), nextLocus.get)
+      val calledAlleles = GermlineStandard.Caller.callVariantsAtLocus(pileup)
+
+      genotypes ++= calledAlleles.flatMap(AlleleConversions.calledAlleleToADAMGenotype(_))
+
+      nextLocus = window.nextLocusWithRegions()
+    }
+    genotypes
   }
 }
