15c1d35a1a14dc913a49ff62c6fec53780c2fce9
arahuja <aahuja11@gmail.com>
2014-12-15 15:41:31 -0500

    Make interval partitioning configurable

--- src/main/scala/org/hammerlab/varcrunch/pipelines/GermlinePipeline.scala
+++ src/main/scala/org/hammerlab/varcrunch/pipelines/GermlinePipeline.scala
@@ -9,12 +9,15 @@ import org.hammerlab.guacamole.variants.AlleleConversions
 import org.hammerlab.guacamole.windowing.SlidingWindow
 import org.hammerlab.varcrunch.VarCrunchArgs
 import org.hammerlab.varcrunch.read.Reads
+import org.kohsuke.args4j.{Option => Opt}
 
 import scala.collection.mutable.ArrayBuffer
 
 object GermlinePipeline extends VarCrunchArgs with PipelineApp {
 
-  type GenomicPosition = (String, Int)
+  @Opt(name = "--intervalLength", aliases = Array("-l"))
+  protected var intervalLength: Int = 100000
+
 
   override def run(args: Array[String]): Unit = {
     super.parseArguments(args)
@@ -23,15 +26,14 @@ object GermlinePipeline extends VarCrunchArgs with PipelineApp {
 
     // Filter to mapped reads
     val mappedReads = reads.filter(_.getReadMapped)
+
     // Compute depth in interval
     val readsInIntervals: PCollection[(String, Long)] = mappedReads.flatMap( alignmentRecord => {
-      val intervalSize = 100000
       val overlappingIntervals =
         Range.Long.
-        inclusive(alignmentRecord.getStart, alignmentRecord.getEnd - 1, 1).map( _ / intervalSize).toSet
+        inclusive(alignmentRecord.getStart, alignmentRecord.getEnd - 1, 1).map( _ / intervalLength).toSet
       overlappingIntervals.map(pos => (alignmentRecord.getContig.getContigName, pos))
     }, Writables.tuple2(Writables.strings, Writables.longs))
-
 
     val intervalCounts = readsInIntervals.count()
 
@@ -43,10 +45,9 @@ object GermlinePipeline extends VarCrunchArgs with PipelineApp {
 
   def callVariants(reads: PCollection[AlignmentRecord]): PCollection[Genotype] = {
     val readsPartitioned: PTable[Int, (Long, AlignmentRecord)] = reads.flatMap(r => {
-      val intervalSize = 100000
       val overlappingIntervals =
         Range.Long.
-          inclusive(r.getStart, r.getEnd - 1, 1).map( _ / intervalSize ).toSet
+          inclusive(r.getStart, r.getEnd - 1, 1).map( _ / intervalLength ).toSet
       overlappingIntervals.map(task => (task.toInt, (r.getStart.toLong, r)))
     })
 
@@ -70,7 +71,6 @@ object GermlinePipeline extends VarCrunchArgs with PipelineApp {
         val calledAlleles = GermlineStandard.Caller.callVariantsAtLocus(pileup)
         genotypes ++= calledAlleles.flatMap(AlleleConversions.calledAlleleToADAMGenotype(_))
       }
-
       nextLocus = window.nextLocusWithRegions()
     }
     genotypes
